e1ed974538ab59efe814608894098ec3ba8655e4
pc45 <pete@petecroke.com>
2021-04-28 12:36:07 -0400

    refactor repeated elements into blade components

---
+++ resources/views/components/game-card-small.blade.php
@@ -0,0 +1,7 @@
+<div class="game flex">
+    <a href="#"><img src="{{ $game['coverImageURL'] }}" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150"></a>
+    <div class="ml-4">
+        <a href="#" class="hover:text-gray-300">{{ $game['name'] }}</a>
+        <div class="text-gray-400 text-sm mt-1">{{ $game['date'] }}</div>
+    </div>
+</div>
